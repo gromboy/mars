cc96491ddde8f25672aa5393f82f60b1eb846f2a
timmka <60577561+gromboy@users.noreply.github.com>
2024-02-05 19:25:14 +0400

    img of mars

--- main.py
+++ main.py
@@ -21,5 +21,20 @@ def promotion():
     <br>И начнем с Марса!<br>Присоединяйся!'''
 
 
+@app.route('/image_mars')
+def image():
+    return f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Привет, Марс!</title>
+</head>
+<body>
+    <h1>Жди нас, Марс!</h1>
+    <img src="{url_for('static', filename='img/mars.png')}">
+</body>
+</html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
