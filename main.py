ee7816c498a070b7cabc3b5d812919e98c3bcbdd
timmka <60577561+gromboy@users.noreply.github.com>
2024-02-05 19:52:41 +0400

    advert + img

--- main.py
+++ main.py
@@ -36,5 +36,40 @@ def image():
 </html>'''
 
 
+@app.route('/promotion_image')
+def prom_img():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                  <h1 style="color: red;">Жди нас, Марс!</h1>
+                  <img src="{url_for('static', filename='img/mars.png')}"/>
+                  
+                  <div class="alert alert-primary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      Присоединяйся!
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
